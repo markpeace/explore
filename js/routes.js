34f331dd64639fa2e88c4d8ee54b754887503a34
markpeace <mark@learnsomestuff.com>
2015-04-08 19:09:44 +0000

    fleshed out menu

--- js/routes.js
+++ js/routes.js
@@ -2,11 +2,76 @@ explore.config(function($stateProvider, $urlRouterProvider) {
 
         $stateProvider
 
-        .state('connect', {
+                .state('connect', {
                 url: '/connect',
                 controller:'Connect',
                 templateUrl: 'pages/connect.html'
         })
+
+                .state('menu', {
+                url: '/menu',
+                abstract: true,
+                //controller:'MainUI',
+                templateUrl: 'pages/mainUI.html'
+        })
+                .state('menu.clues', {
+                url: '/clues',
+                views: {
+                        'centre-panel': {
+                                templateUrl: 'pages/clues.html',
+                        },
+                        'left-panel': {
+                                templateUrl: 'pages/menu.html'
+                        }
+                }
+        })
+         .state('menu.badges', {
+                url: '/badges',
+                views: {
+                        'centre-panel': {
+                                templateUrl: 'pages/badges.html',
+                        },
+                        'left-panel': {
+                                templateUrl: 'pages/menu.html'
+                        }
+                }
+        })
+         .state('menu.notifications', {
+                url: '/notifications',
+                views: {
+                        'centre-panel': {
+                                templateUrl: 'pages/notifications.html',
+                        },
+                        'left-panel': {
+                                templateUrl: 'pages/menu.html'
+                        }
+                }
+        })
+         .state('menu.leaguetable', {
+                url: '/leaguetable',
+                views: {
+                        'centre-panel': {
+                                templateUrl: 'pages/leaguetable.html',
+                        },
+                        'left-panel': {
+                                templateUrl: 'pages/menu.html'
+                        }
+                }
+        })
+         .state('menu.groups', {
+                url: '/groups',
+                views: {
+                        'centre-panel': {
+                                templateUrl: 'pages/groups.html',
+                        },
+                        'left-panel': {
+                                templateUrl: 'pages/menu.html'
+                        }
+                }
+        })
+
+
+
         $urlRouterProvider.otherwise("/connect");
 
 })
