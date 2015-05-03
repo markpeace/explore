21d9f61a8de3653968a474a43f14553d9532c4a3
markpeace <mark@learnsomestuff.com>
2015-05-03 09:17:42 +0000

    basic clue description

--- js/controllers/clueDetail.js
+++ js/controllers/clueDetail.js
@@ -1,4 +1,12 @@
 app.controller('ClueDetail', function($scope, DataService, $stateParams) { 
         console.info("Navigated to Clue Details for " + $stateParams.id)
         $scope.clue = DataService.clues[$stateParams.id];
+        $scope.clue.distance=1
+        
+        $scope.checkinIcons = {
+                "Checkin" : "ion-location",
+                "QR Code" : "ion-qr-scanner",
+                "Selfie" : "ion-person",
+        }
+        
 });
--- js/services/data.js
+++ js/services/data.js
@@ -9,15 +9,19 @@ app.service('DataService', function() {
         for(x=0; x<10; x++) {
                 cache.categories.push({ id: x, label: "Category #" + x })
         }
-        
+               
         //Generate Random Clues        
         for(x=0; x<100; x++) {
                 cache.clues.push({
                         id: x,
-                        title:"Clue #" + x,
+                        title:"Location #" + x,
                         category: cache.categories[Math.round(Math.random() * cache.categories.length)],
+                        enigma: "The enigma is a 'clue' which guides participants towards the site - it should be accessible, but offer challenge",
+                        photo: ["","http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png"][Math.round(Math.random() * 0.75)],
+                        information: "The informatio is the blurb which appears once a participant has found a particular clue - it should offer context and information on the site.",
+                        type: ['Checkin','QR Code','Selfie'][Math.round(Math.random() * 3)],
                         distance: Math.round(Math.random() * 2500),
-                        found: Math.round(Math.random() * 0.75)
+                        found: Math.round(Math.random() * 0.75),
                 })
         }                        
         
