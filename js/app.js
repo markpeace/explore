0b9798a4718b34b0fe7d72cfc50cbf868a58ef13
markpeace <mark@learnsomestuff.com>
2015-05-03 10:52:30 +0000

    updated manifest

--- js/app.js
+++ js/app.js
@@ -1,23 +1,2 @@
-alert(typeof cordova);
-if (typeof cordova === 'object') {
-        document.addEventListener("deviceready", function() {
-                angular.bootstrap(document, ["explore"]);    
-                alert("bootstrapped!");
-
-        });
-
-} else {        
-        angular.element(document).ready(function() {
-                angular.bootstrap(document, ["explore"]);
-        });
-}
-
-var bbb = angular.module('bbb', ['ionic', 'monospaced.qrcode'])
-
-.run(function($rootScope, ParseService, $location, $state) {
-
-        $rootScope.currentUser = Parse.User.current();              
-
-});
-
 app = angular.module('explore', ['ionic'])       
+alert(app);
