15d54242d50ddbbc76bf6cffa78550f5cd1abace
markpeace <mark@learnsomestuff.com>
2015-05-07 08:00:00 +0000

    revert to web geolocation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-try {
-        app = angular.module('explore', ['ionic']);
-} catch(ex){
-        alert(ex);
-}        
+/*if (typeof cordova === 'object') {
+        document.addEventListener("deviceready", function() {
+
+                angular.bootstrap(document, ["explore"]);    
+
+        });
+
+} else {        
+        angular.element(document).ready(function() {
+                angular.bootstrap(document, ["explore"]);
+        });
+}*/
+
+app = angular.module('explore', ['ionic']);
 
 alert(typeof cordova);
--- js/controllers/locations.js
+++ js/controllers/locations.js
@@ -5,7 +5,7 @@ app.controller('Locations', function($scope, DataService) {
         alert("start");
         navigator.geolocation.watchPosition(
                 function(e) { $scope.geoloc = e.coords; $scope.locations.forEach(function(l) {l.updateDistance(e.coords)}) }, 
-                function(e) { alert(e);}, 
+                function(e) { alert(e.message);}, 
                 { maximumAge: 10000, timeout: 10000, enableHighAccuracy: true })                      
 
 });
