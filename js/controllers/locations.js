cdf4619d862ba8a01a52f25ea0b567c55ea320d4
markpeace <mark@learnsomestuff.com>
2015-05-07 10:37:15 +0000

    trying to get geolocation working

--- js/controllers/locations.js
+++ js/controllers/locations.js
@@ -2,13 +2,10 @@ app.controller('Locations', function($scope, DataService) {
         $scope.locations = DataService.locations;          
         $scope.geoloc = {}
 
-        alert("start");
-
         document.addEventListener("deviceready", function() {
-                alert("device ready")
                 navigator.geolocation.watchPosition(
                         function(e) { $scope.geoloc = e.coords; $scope.locations.forEach(function(l) {l.updateDistance(e.coords)}) }, 
-                        function(e) { alert(e.message);}, 
+                        function(e) {}, 
                         { maximumAge: 10000, timeout: 10000, enableHighAccuracy: true })         
         }, false);
 
