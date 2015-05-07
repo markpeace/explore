8800a82ec786a1cce044dd97a321de60d9993897
markpeace <mark@learnsomestuff.com>
2015-05-07 07:52:39 +0000

    revert to web geolocation

--- js/controllers/locations.js
+++ js/controllers/locations.js
@@ -2,6 +2,9 @@ app.controller('Locations', function($scope, DataService) {
         $scope.locations = DataService.locations;          
         $scope.geoloc = {}
 
-        navigator.geolocation.watchPosition(function(e) { $scope.geoloc = e.coords; $scope.locations.forEach(function(l) {l.updateDistance(e.coords)}) }, function() {}, { maximumAge: 10000, timeout: 5000, enableHighAccuracy: true })                      
+        navigator.geolocation.watchPosition(
+                function(e) { $scope.geoloc = e.coords; $scope.locations.forEach(function(l) {l.updateDistance(e.coords)}) }, 
+                function(e) { alert(e);}, 
+                { maximumAge: 10000, timeout: 5000, enableHighAccuracy: true })                      
 
 });
