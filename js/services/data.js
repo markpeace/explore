96006d23342bbd30aae9cd4d7c3dc8029c14564e
markpeace <mark@learnsomestuff.com>
2015-05-17 18:58:04 +0000

    sorting out login

--- js/services/data.js
+++ js/services/data.js
@@ -4,6 +4,24 @@ app.service('DataService', function($q, $state, $ionicLoading) {
         var js_key = "sYzm2V5ylN7nGNlediCexynKV5HyHRQIxtJMXI4N";
         Parse.initialize(app_id, js_key);
 
+        if(user=window.localStorage.getItem("key")) {
+
+        } else {
+                var user = new Parse.User();
+                user.set("username", "my name");
+                user.set("password", "my pass");
+                alert(device.uuid)
+/*
+                user.signUp(null, {
+                        success: function(user) {
+                                // Hooray! Let them use the app now.
+                        },
+                        error: function(user, error) {
+                                // Show the error message somewhere and let the user try again.
+                                alert("Error: " + error.code + " " + error.message);
+                        }
+                });*/
+        }              
 
         //DEFINE MODEL MAKER
         Model = function (options) {
