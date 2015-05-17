8c9c8350fe3ea30ee93129d9dee22e796d766eab
markpeace <mark@learnsomestuff.com>
2015-05-17 16:02:08 +0000

    can now create groups

--- js/controllers/group/editGroup.js
+++ js/controllers/group/editGroup.js
@@ -1,45 +1,15 @@
 app.controller('EditGroup', function($scope, $ionicModal, $ionicPopover, $state, $stateParams, DataService, GeoLocator) { 
         console.info("adding/editing a group");
-/*
 
-        $scope.types = ['GPS', 'QR Code', 'Selfie']       
-
-        $scope.geolocationColor = 'red'
-
-        $scope.takePhoto = function() {
-                navigator.camera.getPicture(function(e) {
-                        $scope.location.image=e
-                }, function() {}, { 
-                        quality : 50,
-                        destinationType : Camera.DestinationType.DATA_URL,
-                        //sourceType : Camera.PictureSourceType.CAMERA,
-                        allowEdit : true,
-                        encodingType: Camera.EncodingType.JPEG,
-                        targetWidth: 400,
-                        targetHeight: 400,
-                        //popoverOptions: CameraPopoverOptions,
-                        saveToPhotoAlbum: false 
-                });
+        if($stateParams.id) {
+                $scope.group = DataService.group.filterBy({id:$stateParams.id})[0]
+        } else {
+                $scope.group = DataService.group.new();   
         }
-
-        $scope.categories = DataService.category.all();
-
-        $ionicModal.fromTemplateUrl('category-popover.html', {
-                scope: $scope,
-                animation: 'slide-in-up'
-        }).then(function(popover) {
-                $scope.categoryChooser = popover;
-        });
-
-        $scope.openCategoryChooser = function() { $scope.categoryChooser.show(); }
-        $scope.closeCategoryChooser = function() { $scope.categoryChooser.hide(); }
-
-
-
-
+        
         $scope.save = function() {
-                $scope.location.save().then(function() {
-                        $state.go("ui.clues");
+                $scope.group.save().then(function() {
+                        $state.go("ui.Groups");
                 }, function (e) {
                         alert(e)
                 });
@@ -48,42 +18,16 @@ app.controller('EditGroup', function($scope, $ionicModal, $ionicPopover, $state,
 
         $scope.delete = function() {
                 var confirmPopup = $ionicPopup.confirm({
-                        title: 'Delete Location',
-                        template: 'Are you sure you want to delete this location?'
+                        title: 'Delete Group',
+                        template: 'Are you sure you want to delete this group (all memberships will also be deleted)?'
                 });
                 confirmPopup.then(function(res) {
                         if(res) {
-                                $scope.location.delete().then(function() {
-                                        $state.go("ui.clues");
+                                $scope.location.group.delete().then(function() {
+                                        $state.go("ui.Groups");
                                 });
                         }
                 });
         }
-
-        $scope.triggerGeolocation = function() {
-                console.log("looking for geolocation"); 
-                GeoLocator.go({
-                        scope:$scope,
-                        success: function(e) {
-                                $scope.location.geolocation = e.coords
-                                if (e.coords.accuracy>30) {
-                                        $scope.geolocationColor = 'red'
-                                } else if (e.coords.accuracy>15) {
-                                        $scope.geolocationColor = 'orange'
-                                } else {
-                                        $scope.geolocationColor = 'green'
-                                }                        
-                        }
-                })
-        }
-
-
-        if($stateParams.id) {
-                $scope.location = DataService.location.filterBy({id:$stateParams.id})[0]
-        } else {
-                $scope.location = DataService.location.new({ type: 'GPS' });   
-                $scope.triggerGeolocation();
-        }
-*/
-
+      
 });
