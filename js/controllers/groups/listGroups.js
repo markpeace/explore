ff6f3838a66b99adac95b39704f2b334bae98149
markpeace <mark@learnsomestuff.com>
2015-05-17 15:33:48 +0000

    stubbed create group

--- js/controllers/groups/listGroups.js
+++ js/controllers/groups/listGroups.js
@@ -2,14 +2,6 @@ app.controller('ListGroups', function($scope, DataService, GeoLocator) {
        
         console.info("groups view")
         
-        /*$scope.locations = DataService.location;          
-        $scope.locationIndicator = "*";       
-        
-        GeoLocator.go({
-                scope:$scope,
-                success: function(e) {
-                        $scope.locationIndicator = "";
-                        $scope.locations.all().forEach(function(l) {l.updateDistance(e.coords)})
-                }
-        });*/
+        $scope.groups = DataService.group.all()
+       
 });
