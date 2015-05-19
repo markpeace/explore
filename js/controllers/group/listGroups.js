4ae3ef25764bbc6af83610e455f577376e4d51b1
markpeace <mark@learnsomestuff.com>
2015-05-19 08:11:16 +0000

    basic qr code scanning

--- js/controllers/group/listGroups.js
+++ js/controllers/group/listGroups.js
@@ -1,7 +1,21 @@
 app.controller('ListGroups', function($scope, DataService, GeoLocator) { 
-       
+
         console.info("groups view")
-        
+
         $scope.groups = DataService.group.all()
-       
+
+        $scope.joinGroup = function() {
+                try {
+
+                        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+                        scanner.scan(function (result) {
+                                alert(result)
+                        })
+
+                } catch (ex) {
+                        console.log(ex)
+                }
+        }
+
 });
