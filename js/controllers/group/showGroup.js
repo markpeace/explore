f9950d322d70d46e311cada85f8731691d498bc6
markpeace <mark@learnsomestuff.com>
2015-05-23 20:04:15 +0000

    associating groups with priveldges

--- js/controllers/group/showGroup.js
+++ js/controllers/group/showGroup.js
@@ -3,6 +3,8 @@ app.controller('ShowGroup', function($scope, DataService, $stateParams, GeoLocat
 
         $scope.width= $window.innerWidth<$window.innerHeight ? $window.innerWidth : $window.innerHeight * .9
         $scope.width = $scope.width *.9
-
+               
         $scope.group = DataService.group.filterBy({id: $stateParams.id})[0];        
+        $scope.securityLevel = DataService.user.all()[0].securityLevel()
+
 });
