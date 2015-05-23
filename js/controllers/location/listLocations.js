e3fcb546aca2a3fccaa87c62596081391ad19752
markpeace <mark@learnsomestuff.com>
2015-05-23 20:27:06 +0000

    associating groups with priveldges

--- js/controllers/location/listLocations.js
+++ js/controllers/location/listLocations.js
@@ -1,8 +1,20 @@
 app.controller('ListLocations', function($scope, DataService, GeoLocator) { 
         $scope.locations = DataService.location;          
         $scope.locationIndicator = "*";    
-        $scope.securityLevel = DataService.user.all()[0].securityLevel()
-        
+
+        $scope.securityLevel = 9999
+
+        if (DataService.user.all()[0]) {
+                $scope.securityLevel = DataService.user.all()[0].securityLevel()                
+
+        } else {
+                window.setTimeout(function() {
+                        $scope.securityLevel = DataService.user.all()[0].securityLevel()                
+
+                },5000)                
+        }
+
+
         GeoLocator.go({
                 scope:$scope,
                 success: function(e) {
