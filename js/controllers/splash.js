9db62977da8819d39b6a6d1456ae2393dc9480e9
markpeace <mark@learnsomestuff.com>
2015-06-29 15:15:55 +0000

    fixed splash page

--- js/controllers/splash.js
+++ js/controllers/splash.js
@@ -1,26 +1,25 @@
 app.controller('Splash', function($scope, $state, $ionicSideMenuDelegate, DataService) { 
-        $scope.DataService = DataService
-
-        $scope.updateComplete = DataService
-
-        $scope.$watch('updateComplete.isComplete', function(oldval,newval) {
-                if (DataService.isComplete) {
-                        window.setTimeout(function() {                                                                
-                                if(DataService.user.all()[0].groups.all().length>0) {
-                                        $state.go('ui.Locations');
-                                }                       
-                        },300)
-                }
-        })
 
         $scope.joinGroup = function() { 
 
                 try {
-                        DataService.user.all()[0].joinGroup().then(function() {
+                        DataService.user.joinGroup().then(function() {
                                 $state.go('ui.Locations');
                         })
                 } catch(ex) {
                         alert(ex.message)
                 }
         }
+
+        var fetchData = function () {
+                if(DataService.user.groups.data.length>0) {
+                        $state.go('ui.Locations');
+                }                       
+
+        }
+
+        $scope.$on('DataService:DataLoaded', fetchData)        
+        if(DataService._loadcomplete) fetchData();
+
+
 });
