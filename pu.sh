d4ab289815702928c9d2625186016484e1a7cc1a
markpeace <mark@learnsomestuff.com>
2015-07-06 14:02:45 +0000

    testing new pu.sh

--- pu.sh
+++ pu.sh
@@ -1,5 +1,10 @@
+cp config.xml www
+cp -r resources www
+
 git add .
 git commit -a -m "$*"
 git clean -f
-git push origin master
+git push origin master 
 ionic upload
+
+curl -u mark@learnsomestuff.com -X PUT -d 'data={"pull":"true"}' https://build.phonegap.com/api/v1/apps/1532972
