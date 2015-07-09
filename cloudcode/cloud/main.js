173aa8eb8d68bcbe1e9327b9598b91396eed9a89
markpeace <mark@learnsomestuff.com>
2015-07-09 16:37:29 +0000

    messing around with cloud code

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -2,8 +2,10 @@ Parse.Cloud.afterSave("Group", function(request) {
 
         if(!request.object.get("securityLevel")) { request.object.set("securityLevel", 9999).save(); }
 
-        if()        
-        request.object.set("cloudcode", "YES")
-        request.object.save();
+        if(request.object.get("securityLevel")<9999) {
 
+                request.object.set("cloudcode", "YES")
+                request.object.save();
+
+        }       
 });
