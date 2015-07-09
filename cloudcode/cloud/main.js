fc0ba4c9033d29a67be57788618274dd820725b6
markpeace <mark@learnsomestuff.com>
2015-07-09 18:01:35 +0000

    messing around with cloud code

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -1,13 +1,5 @@
 Parse.Cloud.afterSave("Group", function(request) {
 
-        if(!request.object.get("securityLevel")) { request.object.set("securityLevel", 9999).save(); }
-
-        if(request.object.get("securityLevel")<9999) {
-
-                addRole(request.object)
-
-        }       
-
         addRole = function(group) {
 
                 (new Parse.Query(Parse.Role))
@@ -64,5 +56,13 @@ Parse.Cloud.afterSave("Group", function(request) {
 
         }
 
+
+        if(!request.object.get("securityLevel")) { request.object.set("securityLevel", 9999).save(); }
+
+        if(request.object.get("securityLevel")<9999) {
+
+                addRole(request.object)
+
+        }       
 
 });
