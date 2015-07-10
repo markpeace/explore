8892f1cd122512d230b7801ff06cca3f45516fed
markpeace <mark@learnsomestuff.com>
2015-07-10 18:33:18 +0000

    creating a function to increment group checkin count

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.afterSave("Checkin", function(request) {
         Parse.Cloud.useMasterKey();
         
         request.object.get("user").fetch().then(function(user) {
-                console.log(user)
+                console.log("user")
         })
         
 })
