674c5c30bb550a4f8462577e0ac91864dfe3ba3c
markpeace <mark@learnsomestuff.com>
2015-07-15 17:28:07 +0000

    basic league tables working

--- www/js/controllers/league/showLeague.js
+++ www/js/controllers/league/showLeague.js
@@ -1,6 +1,9 @@
-app.controller('ShowLeague', function($scope, $state, $stateParams, DataService) { 
+app.controller('ShowLeague', function($scope, $q, $state, $stateParams, DataService) { 
+                
         var fetchData = function () {
-                //STUFF HERE
+                $scope.league=DataService.league.filterBy({id: $stateParams.id})[0]                
+                $scope.groups = $scope.league.groups.data
+                $scope.groups.forEach(function(group) { group.fetch() })
         }
 
         $scope.$on('DataService:DataLoaded', fetchData)        
