28b23cdf6da02e678d0364ad19e6702264fb37db
markpeace <mark@learnsomestuff.com>
2015-07-20 19:52:57 +0000

    fixing league list bug

--- www/js/controllers/league/listLeagues.js
+++ www/js/controllers/league/listLeagues.js
@@ -4,6 +4,7 @@ app.controller('ListLeagues', function($scope, $state, $stateParams, DataService
         
         var fetchData = function () {
                 DataService.user.groups.data.forEach(function(group) {
+                        alert(group)
                         group.leagues.data.forEach(function(league) {
                                 $scope.leagues.push(league)
                         })
