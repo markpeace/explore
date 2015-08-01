ee1359abc387434c3a81b8c536058b62cb6db951
markpeace <mark@learnsomestuff.com>
2015-08-01 09:16:49 +0000

    messing with geolocation and cordova plugin

--- www/js/services/geolocator.js
+++ www/js/services/geolocator.js
@@ -19,7 +19,7 @@ app.service('GeoLocator', function($cordovaGeolocation) {
                                 console.error("you must pass a scope variable to the geolocation watcher");    
                                 return;
                         }
-                                               
+                        /*          
                         var locationWatcher = $cordovaGeolocation.watchPosition(
                                 params.success,                                                                                  
                                 params.error,
@@ -32,7 +32,25 @@ app.service('GeoLocator', function($cordovaGeolocation) {
 
                         params.scope.$on('$stateChangeStart', function() {                                
                                 navigator.geolocation.clearWatch(locationWatcher);
-                        })      
+                        })      */
+
+
+                        var locationWatcher = $cordovaGeolocation.watchPosition({
+                                maximumAge: 1000, 
+                                timeout: 5000, 
+                                enableHighAccuracy: false
+                        });
+
+                        locationWatcher.then(
+                                null,
+                                params.error,
+                                params.success);
+
+                        params.scope.$on('$stateChangeStart', function() {                                
+                                locationWatcher.clearWatch();
+                        }) 
+
+                        
 
                 }}
 
