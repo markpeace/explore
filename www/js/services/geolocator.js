d719662ea70b8250c4e164295841be32a1908f63
markpeace <mark@learnsomestuff.com>
2015-08-02 15:31:48 +0000

    possibly fixed the problem with geolocation

--- www/js/services/geolocator.js
+++ www/js/services/geolocator.js
@@ -3,40 +3,37 @@ app.service('GeoLocator', function() {
         locationWatcher = {}
 
         return {
+
                 go: function (params) {
 
                         defaults = {
-                                maximumAge:3000,
-                                timeout: 5000,
+                                maximumAge:0,
+                                timeout: 10000,
                                 enableHighAccuracy:true,
                                 success: function(e) { console.info("geolocation updated:" + e); },
-                                error: function(e) { console.warn("geolocation error: "+ e.message) }
+                                error: function(e) { console.log("geolocation error: "+ e.message); alert("geolocation error: "+ e.message) }
                         }
 
+                        var params;
                         for (var attrname in defaults) { params[attrname] = params[attrname] || defaults[attrname]; }
 
                         if (!params.scope) {
                                 console.error("you must pass a scope variable to the geolocation watcher");    
                                 return;
                         }
-                            
-                        var locationWatcher = navigator.geolocation.watchPosition(
-                                params.success,                                                                                  
-                                params.error,
-                                {
-                                        maximumAge: 1000, 
-                                        timeout: 5000, 
-                                        enableHighAccuracy: true
-                                }
-                        )
 
-                        params.scope.$on('$stateChangeStart', function() {                                
-                                navigator.geolocation.clearWatch(locationWatcher);
-                        })      
 
-                    
+                        var repoll = function () {
+                                setInterval(function() {
+                                        navigator.geolocation.getCurrentPosition(params.success, params.error, params)
+                                }, 5000)
+                        }
 
+                        locationWatcher = navigator.geolocation.getCurrentPosition(repoll, null, params)
                         
+                        params.scope.$on('$stateChangeStart', function() {                                
+                                clearInterval(locationWatcher);
+                        })      
 
                 }}
 
