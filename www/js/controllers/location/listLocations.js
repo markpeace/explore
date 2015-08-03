2a8efaa31bcd99f8db6f6263b16d9306737412cd
markpeace <mark@learnsomestuff.com>
2015-08-03 19:27:45 +0000

    can now filter locations by distance

--- www/js/controllers/location/listLocations.js
+++ www/js/controllers/location/listLocations.js
@@ -3,8 +3,15 @@ app.controller('ListLocations', function($scope, $q, DataService, GeoLocator) {
         $scope.geocount = 0;    
         $scope.accuracy = null;
         $scope.securityLevel = 9999
-        $scope.now = Date.now()
 
+        $scope.filters = [
+                { label: "Nearby", min:0, max:500 },
+                { label: "Walkable", min:500, max:1000 },
+                { label: "Distant", min: 1000, max:99999 },
+        ]
+
+        $scope.filter=$scope.filters[0]
+        
         var fetchData = function () {
                 $scope.securityLevel = DataService.user.securityLevel()
                 $scope.locations = DataService.location.all()
@@ -12,12 +19,12 @@ app.controller('ListLocations', function($scope, $q, DataService, GeoLocator) {
 
         $scope.$on('DataService:DataLoaded', fetchData)        
         if(DataService._loadcomplete) fetchData();
-       
+
         GeoLocator.update(function(e) {
-                        $scope.geoerror=null;
-                        $scope.geocount = $scope.geocount+1
-                        $scope.accuracy = e.coords.accuracy
-                        $scope.locations.forEach(function(l) {l.updateDistance(e.coords)})                        
+                $scope.geoerror=null;
+                $scope.geocount = $scope.geocount+1
+                $scope.accuracy = e.coords.accuracy
+                $scope.locations.forEach(function(l) {l.updateDistance(e.coords)})                        
         })               
 
         GeoLocator.ErrorFunction(function(e){
